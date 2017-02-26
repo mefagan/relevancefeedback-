68b94c9f4f2c9a9920a28f9f8c3e4adbfe1d6dfb
MaryEileen Fagan <maryeileen.fagan@gmail.com>
2017-02-26 17:20:30 -0500

    maybe the indexer is working who knows

--- indexer.py
+++ indexer.py
@@ -6,18 +6,41 @@ from lucene import \
     Document, Field, StandardAnalyzer, IndexWriter,  IndexWriterConfig, Version
 
 if __name__ == "__main__":
+    #initialize lucene and jvm
+
     lucene.initVM()
-    dir = SimpleFSDirectory(File("index/"))
+    
+    indexDir = "/Tmp/REMOVEME.index-dir"
+
+    print "lucene version is:", lucene.VERSION
+    
+    #get the analyzer
     analyzer = StandardAnalyzer(Version.LUCENE_30)
+    
+    #get index storage
+    dir = lucene.SimpleFSDirectory(lucene.File(indexDir))
+   
     writer = IndexWriter(dir, analyzer, True, IndexWriter.MaxFieldLength(512))
     
     print "%d docs in index" % writer.numDocs()
     print "Reading lines from sys.stdin..."
 
-    for n, l in enumerate(sys.stdin):
+    i = 0
+    print >> sys.stderr, "Reading lines from sys.stdin..."
+    for l in sys.stdin:
+        i += 1
+        
+        
         doc = Document()
         doc.add(Field("text", l, Field.Store.YES, Field.Index.ANALYZED))
         writer.addDocument(doc)
-    print "Indexed %d lines from stdin (%d documents in index)" % (n, writer.numDocs())
-    print "Closing index of %d docs..." % writer.numDocs()
+        
+        if i % 10000 == 0:
+            print >> sys.stderr, "Read %d lines from stdin (%d documents in index)..." % (i, writer.numDocs())
+    print >> sys.stderr, "Indexed a total of %d lines from stdin (%d documents in index)" % (i, writer.numDocs())
+    print >> sys.stderr, "About to optimize index of %d documents..." % writer.numDocs()
+    writer.optimize()
+    print >> sys.stderr, "...done optimizing index of %d documents" % writer.numDocs()
+    print >> sys.stderr, "Closing index of %d documents..." % writer.numDocs()
     writer.close()
+    print >> sys.stderr, "...done closing index of %d documents" % writer.numDocs()
--- retriever.py
+++ retriever.py
@@ -1,22 +1,16 @@
-import sys
 import lucene
-
-from java.io import File
-from org.apache.lucene.analysis.standard import StandardAnalyzer
-from org.apache.lucene.document import Document, Field
-from org.apache.lucene.search import IndexSearcher
-from org.apache.lucene.index import IndexReader
-from org.apache.lucene.queryparser.classic import QueryParser
-from org.apache.lucene.store import SimpleFSDirectory
-from org.apache.lucene.util import Version
+from lucene import \
+    SimpleFSDirectory, System, File, \
+    Document, Field, StandardAnalyzer, IndexSearcher, Version, QueryParser
 
 if __name__ == "__main__":
     lucene.initVM()
-    analyzer = StandardAnalyzer(Version.LUCENE_4_10_1)
-    reader = IndexReader.open(SimpleFSDirectory(File("index/")))
-    searcher = IndexSearcher(reader)
+    indexDir = "/Tmp/REMOVEME.index-dir"
+    dir = SimpleFSDirectory(File(indexDir))
+    analyzer = StandardAnalyzer(Version.LUCENE_30)
+    searcher = IndexSearcher(dir)
     
-    query = QueryParser(Version.LUCENE_4_10_1, "text", analyzer).parse("Find this sentence please")
+    query = QueryParser(Version.LUCENE_30, "text", analyzer).parse("Find this sentence please")
     MAX = 1000
     hits = searcher.search(query, MAX)
     
