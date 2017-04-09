689ca733cdaf77b9901fc610228214188d851027
MaryEileen Fagan <maryeileen.fagan@gmail.com>
2017-04-09 10:32:56 -0400

    first commit

---
+++ sourcecode/indexTest.py
@@ -0,0 +1,31 @@
+from htmlparser import parsehtml
+import lucene
+import os
+from lucene import SimpleFSDirectory, System, File, Document, Field, StandardAnalyzer, IndexWriter, Version
+if __name__ == "__main__":
+    lucene.initVM()
+    src_dir = "html_files"
+    indexDir = "/Tmp/REMOVEME.index-dir"
+    dir = SimpleFSDirectory(File(indexDir))
+    analyzer = StandardAnalyzer(Version.LUCENE_30)
+    writer = IndexWriter(dir, analyzer, True, IndexWriter.MaxFieldLength(512))
+    print ("Currently there are %d documents in the index..." % writer.numDocs())
+    print ("Reading lines from directory...")
+    i = 0
+    for l in os.listdir(src_dir):
+        l = os.path.join(src_dir, l)
+        with open(l, 'r') as myfile:
+            data=myfile.read()
+        document, errors = parsehtml(data)
+        print(l)
+        i += 1
+        doc = Document()
+        doc.add(Field("text", document, Field.Store.YES, Field.Index.ANALYZED))
+        writer.addDocument(doc)
+    print ("Indexed lines from stdin (%d documents in index)" % (writer.numDocs()))
+    print ("About to optimize index of %d documents..." % writer.numDocs())
+    writer.optimize()
+    print ("...done optimizing index of %d documents" % writer.numDocs())
+    print ("Closing index of %d documents..." % writer.numDocs())
+    writer.close()
+    print ("...done closing index of %d documents" % writer.numDocs())
---
+++ sourcecode/run.py
@@ -0,0 +1,25 @@
+import subprocess
+
+def BuildSearchEngine(url, number, domain):
+    p1 = subprocess.Popen(["python3", "crawler.py", url, str(number), domain])
+    p1.wait()
+    p2 = subprocess.Popen(["python2.6", "indexTest.py"])
+    p2.wait()
+    # 90 is the timeout in seconds
+    try:
+        out_1, errs_1 = p1.communicate(timeout=500)
+    
+    except subprocess.TimeoutExpired:
+        p1.kill()
+        out_1, errs_1 = p1.communicate()
+    
+    try:
+        out_2, errs_2 = p2.communicate(timeout=180)
+    except subprocess.TimeoutExpired:
+        p2.kill()
+        out_2, errs_2 = p2.communicate()
+
+def main():
+    BuildSearchEngine("https://en.wikipedia.org/", 25, "wikipedia.org")
+if __name__ == '__main__':
+    main()
