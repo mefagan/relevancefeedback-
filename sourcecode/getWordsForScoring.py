a83529dcce714d0747ddc09a3f4f89d96ea40b02
MaryEileen Fagan <maryeileen.fagan@gmail.com>
2017-04-13 12:01:28 -0400

    added function to get list of all unique words in files that are not in original query

---
+++ sourcecode/getWordsForScoring.py
@@ -0,0 +1,22 @@
+#http://stackoverflow.com/questions/6181763/converting-a-string-to-a-list-of-words
+#http://stackoverflow.com/questions/7794208/how-can-i-remove-duplicate-words-in-a-string-with-python
+import os
+import re
+def getWordsForScoring(query):
+	query_words = query.lower().split()
+
+	#get a list of all unique words in documents
+	ulist = []
+
+	path = 'noStopWords_files'
+	for filename in os.listdir(path):
+		with open(os.path.join(path, filename), 'r') as myfile:
+			data=myfile.read()
+			wordList = re.sub("[^\w]", " ",  data).split()
+			[ulist.append(x) for x in wordList if x not in ulist]
+
+		#removed_query_words = ' '.join([word for word in text.split() if word not in stop_words])
+def main():
+	getWordsForScoring("Wikipedia")
+if __name__ == '__main__':
+	main()
