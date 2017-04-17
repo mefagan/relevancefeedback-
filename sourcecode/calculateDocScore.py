f65a15423734e450a64803ecc00fecacf71080ad
MaryEileen Fagan <maryeileen.fagan@gmail.com>
2017-04-17 12:06:48 -0400

    counting five words f(W,Q,D) works

--- sourcecode/calculateDocScore.py
+++ sourcecode/calculateDocScore.py
@@ -14,12 +14,31 @@ def calculateDocScore(doc, word, q):
 	print(lD)
 	cWD = 0
 	mQD = 0
+	fWQD = 0
 
 
 	for i in range(len(words)):
 		w = words[i]
 		if w==word:
 			cWD = cWD + 1
+			start = max(0, i-5)
+			end = min(len(words)-1, i+5)
+			print(end)
+			j = start
+			k = 0
+			while (j<end+1):
+				for q in query_words:
+					if words[j]==q:
+						
+						k = k+1
+				
+				j = j+1
+			if k>1:
+				
+				fWQD = fWQD+1
+				
+				k = 0
+
 		for query in query_words:
 			
 			if query==w:
@@ -28,3 +47,5 @@ def calculateDocScore(doc, word, q):
 	print(cWD)
 	print("mQDm which is number of occurances of any query word in doc = ")
 	print(mQD)
+	print("fWQD = ")
+	print(fWQD)
