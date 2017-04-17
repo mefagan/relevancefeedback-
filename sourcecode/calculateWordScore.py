5d806a0aed56ad5e7902f0e32cf88fa74c8a577c
MaryEileen Fagan <maryeileen.fagan@gmail.com>
2017-04-17 08:56:25 -0400

    calculating g(W) and z(W, Q) both working

--- sourcecode/calculateWordScore.py
+++ sourcecode/calculateWordScore.py
@@ -1,4 +1,9 @@
 from calczWQ import calczWQ
+from calcGW import calcGW
 def calculateWordScore(word):
-	i = calczWQ(word)
- 	print(i)
+	gW = calcGW(word)
+	zWQ = calczWQ(word)
+ 	print("g(W) = ")
+ 	print(gW)
+ 	print("z(W, Q) = ")
+ 	print(zWQ)
